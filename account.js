588dccce4d89936589ce09eafaa73b6282c0a546
Alexander Alie <alexanderalie@alexanders-mbp.home>
2021-03-30 20:46:46 -0400

    updates

--- account.js
+++ account.js
@@ -5,10 +5,10 @@ const login = require('./login.js');
 
 const accountUser = (user) => {
   const iUser = user.user_name;
-  console.log(user.user_name)
+  // console.log(user.user_name)
 
   const iUserID = user.id
-  console.log(iUserID)
+  // console.log(iUserID)
   inquirer.prompt([
     {
       type: "list",
@@ -19,19 +19,19 @@ const accountUser = (user) => {
   ]).then(function(profile){
     if(profile.accountOptions == 'View Songs'){
       console.log("Your Songs")
-      viewPlaylist(iUserID);
+      viewPlaylist(user);
     }else if(profile.accountOptions == 'Add Song'){
       // console.log("Added Song");
-      addSong(iUserID);
+      addSong(user);
     }else{
       console.log("Goodbye");
     }
   })
 }
 
-function addSong(iUserID){
+function addSong(user){
   let buyMusic = "SELECT songs.id FROM songs INNER JOIN bought_songs ON bought_songs.song_id = songs.id WHERE bought_songs.user_id="
-  buyMusic += "'"+iUserID+"'"
+  buyMusic += "'"+user+"'"
   db.query(buyMusic, function(err, data){
     let ownedSongs = data.map((song) => song.id)
     console.log(ownedSongs);
@@ -63,11 +63,23 @@ function addSong(iUserID){
           }
         ]).then(function(music){
           if(music.songChoices != "Back"){
-            console.log("Succesfully Purchased")
-            console.log(music.songChoices);
+            db.query("SELECT id FROM songs WHERE artist = '"+music.songChoices.split(":")+"'", function(err5, data5){
+              if(err){
+                throw new Error(err5);
+              }
+              console.log(data5)
+              // db.query("INSERT INTO bought_songs (user_id, song_id) VALUES ("+user.id+", "+data5[0].id+")", function(err6, data6){
+              //   if(err){
+              //     throw new Error(err6)
+              //   }
+              //   console.log("Succesfully Purchased")
+              //   // console.log(music.songChoices);
+              //   accountUser(user);
+              // })
+            })
           }else{
             console.log("Going Back");
-            accountUser(iUserID);
+            accountUser(user);
           }
         })
       })
@@ -76,12 +88,12 @@ function addSong(iUserID){
 };
 
 
-function viewPlaylist(iUserID){
-  db.query("SELECT * FROM songs INNER JOIN bought_songs ON bought_songs.song_id = songs.id WHERE bought_songs.user_id =" +iUserID, function(err4, data4){
+function viewPlaylist(user){
+  db.query("SELECT * FROM songs INNER JOIN bought_songs ON bought_songs.song_id = songs.id WHERE bought_songs.user_id =" +user.id, function(err4, data4){
     data4.forEach((pSongs, i) => {
       console.log((i + 1) + "." + pSongs.song_name + "," + pSongs.artist);
     })
-    accountUser(iUserID);
+    accountUser(user);
   })
 }
 
